1c3421dbc3ec6cfe4ea61152a2e97f2d4f1f8e79
skylinezy <developer.yuanzi@gmail.com>
2022-03-31 21:40:45 -0400

    Initial Commit

---
+++ zPhoto/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  zPhoto
+//
+//  Created by Ryan Zi on 3/31/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ zPhoto/zPhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  zPhotoApp.swift
+//  zPhoto
+//
+//  Created by Ryan Zi on 3/31/22.
+//
+
+import SwiftUI
+
+@main
+struct zPhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
